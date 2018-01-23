529a2794b6b04fc45a6f171d173b79e90a59cb9d
devDandy <dschnd@gmail.com>
2018-01-23 13:14:44 -0600

    Added files

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>DMACC PORTFOLIO DAY</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+</head>
+<body>
+	<div class="wrapper">
+		
+		<header>
+			<div class="row">
+				<div class="col-12 navBG">
+					<nav>
+						<ul class="desktopMenu">
+							<li class="nav-lead"><a href="#">DMACC Portfolio Day</a></li>
+							<li><a href="#">Graphic Design</a> </li>
+							<li><a href="#">Photography</a></li>
+							<li><a href="#">Web Development</a> </li>
+							<li><a href="#">Video Production</a> </li>
+							<li><a href="#">Animation</a></li>
+						</ul>
+						<div  class="mobileNav">
+							<a href="#" class="navLogo">DMACC Portfolio Day</a>
+							<br>
+							<span onclick="openNav()" class="openHamburgerMenu">
+								Menu
+								<img src="assets/img/menu.png">
+							</span>
+						</div>
+						<div class="mobileMenu" id="mobileMenu">
+  							<a href="javascript:void(0)" class="closebtn" onclick="closeNav()"> &times; </a>
+							<a href="#">DMACC Portfolio Day</a> 
+							<a href="#">Graphic Design</a> 
+							<a href="#">Photography</a>
+							<a href="#">Web Development</a> 
+							<a href="#">Video Production</a> 
+							<a href="#">Animation</a>
+						</div>
+					</nav>
+				</div>
+			</div>
+			<div class="headerContainer">
+			<div class="row">
+				<div class="col-md-6 col-sm-12">
+						<div class="mainHeaderContent">
+							<img class="dmaccPortfolioDayLogo" src="assets/img/dmaccPortfolioDayLogo.svg">
+						</div>
+				</div>
+				<div class="col-md-6 col-sm-12">
+					<div class="countdownContainer">
+						<div class="mainHeaderContent">
+							<div class="mainHeaderContentCentered">
+								<div id="portfolioDayCountDown"></div>
+								<p>until the awesomeness begins.</p>
+							</div>
+
+						</div>
+					</div>
+
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12">
+					<div class="portfolioDayInformation">
+						<h2>Thursday, April 12, 2018</h2>
+						<h2>Noon - 5:00 P.M.</h2>
+						<h2>Greater Des Moines Botanical Garden</h2>
+						<p class="lead"></p>
+						<div class="socialMediaList">
+								<div class="socialMediaColumn">
+									<a href="https://www.facebook.com/dmaccportfolioday/"><img src="assets/img/icons/facebook.png"></a>
+								</div>
+								<div class="socialMediaColumn">
+									<a href="https://www.instagram.com/dmaccportfolioday/"><img src="assets/img/icons/instagram1.png"></a>
+								</div>
+						</div>				
+				</div>
+				</div>
+			</div>
+		</div>
+		</header>
+		<main>
+		</main>
+	</div>
+	<!-- wrapper -->
+	<div class="pushFooter"></div>
+	<footer >
+		<div class="miscellaneousInfo">
+			<p class="small">Font developed and designed by <a href="https://www.behance.net/bakoom-studio">Bakoom Studio</a></p>
+			<p class="small">Des Moines Area Community College - Ankeny, IA</p>
+			<p class="small">Copyright 2017 DMACC Portfolio Day</p>
+		</div>
+
+	</footer>
+    <!-- Optional JavaScript -->
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/countdown.js"></script>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</body>
+</html>

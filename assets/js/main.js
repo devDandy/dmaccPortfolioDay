af5d28c99d0ed02e448b9451405923f4f7fac073
Dan Schneider <Dschnd@gmail.com>
2018-01-23 19:22:48 -0600

    Fixed mobile menu not opening in mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mobileMenu").style.width = "100%";
+    document.getElementById("mobile-menu").style.width = "100%";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("mobileMenu").style.width = "0";
+    document.getElementById("mobile-menu").style.width = "0";
 } 
